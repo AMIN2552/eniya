911d709bf910319099200d4b4b0a34029f94cf12
user <amin7061205@gmail.com>
2022-08-28 12:59:47 +0300

    Initial commit

---
+++ src/eniya.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class eniya {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int calc = (a * b * c);
+        //int mul =(a * b * c);
+        System.out.println(calc * 2);
+    }
+}
